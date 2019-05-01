e33bee24588479b5ef19f570c0cd80298f2cd5d1
ace2nutzer <djsilvio1984@gmail.com>
2019-05-01 21:36:55 +0200

    fs/sdfat/blkdev: disable debugging

--- fs/sdfat/blkdev.c
+++ fs/sdfat/blkdev.c
@@ -96,7 +96,9 @@ s32 bdev_check_bdi_valid(struct super_block *sb)
 			fsi->prev_eio |= SDFAT_EIO_BDI;
 			sdfat_log_msg(sb, KERN_ERR, "%s: block device is "
 				"eliminated.(bdi:%p)", __func__, sb->s_bdi);
+#ifdef CONFIG_SDFAT_DEBUG
 			sdfat_debug_warn_on(1);
+#endif
 		}
 		return -ENXIO;
 	}
@@ -159,7 +161,9 @@ s32 bdev_mread(struct super_block *sb, u64 secno, struct buffer_head **bh, u64 n
 	if (!(fsi->prev_eio & SDFAT_EIO_READ)) {
 		fsi->prev_eio |= SDFAT_EIO_READ;
 		sdfat_log_msg(sb, KERN_ERR, "%s: No bh. I/O error.", __func__);
+#ifdef CONFIG_SDFAT_DEBUG
 		sdfat_debug_warn_on(1);
+#endif
 	}
 
 	return -EIO;
@@ -213,7 +217,9 @@ no_bh:
 	if (!(fsi->prev_eio & SDFAT_EIO_WRITE)) {
 		fsi->prev_eio |= SDFAT_EIO_WRITE;
 		sdfat_log_msg(sb, KERN_ERR, "%s: No bh. I/O error.", __func__);
+#ifdef CONFIG_SDFAT_DEBUG
 		sdfat_debug_warn_on(1);
+#endif
 	}
 
 	return -EIO;
