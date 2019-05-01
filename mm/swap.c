0027ee1c001eaa70927efbd2f1f4716c887581f8
ace2nutzer <djsilvio1984@gmail.com>
2019-05-01 18:26:40 +0200

    mm/swap: page_cluster = 0

--- mm/swap.c
+++ mm/swap.c
@@ -1144,7 +1144,7 @@ EXPORT_SYMBOL(pagevec_lookup_tag);
  */
 void __init swap_setup(void)
 {
-	unsigned long megs = totalram_pages >> (20 - PAGE_SHIFT);
+//	unsigned long megs = totalram_pages >> (20 - PAGE_SHIFT);
 #ifdef CONFIG_SWAP
 	int i;
 
@@ -1153,10 +1153,10 @@ void __init swap_setup(void)
 #endif
 
 	/* Use a smaller cluster for small-memory machines */
-	if (megs < 16)
-		page_cluster = 2;
-	else
-		page_cluster = 3;
+//	if (megs < 16)
+		page_cluster = 0;
+//	else
+//		page_cluster = 3;
 	/*
 	 * Right now other parts of the system means that we
 	 * _really_ don't want to cluster much more
