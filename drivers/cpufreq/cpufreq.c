5636aa295b84e2662adfdf762fcd2f5471f507da
ace2nutzer <djsilvio1984@gmail.com>
2019-05-17 04:58:46 +0200

    OC CPU/GPU

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -745,8 +745,6 @@ static ssize_t store_##file_name					\
 	return ret ? ret : count;					\
 }
 
-store_one(scaling_min_freq, min);
-store_one(scaling_max_freq, max);
 store_one(user_scaling_min_freq, min);
 store_one(user_scaling_max_freq, max);
 
@@ -921,8 +919,8 @@ cpufreq_freq_attr_ro(scaling_cur_freq);
 cpufreq_freq_attr_ro(bios_limit);
 cpufreq_freq_attr_ro(related_cpus);
 cpufreq_freq_attr_ro(affected_cpus);
-cpufreq_freq_attr_rw(scaling_min_freq);
-cpufreq_freq_attr_rw(scaling_max_freq);
+cpufreq_freq_attr_ro(scaling_min_freq);
+cpufreq_freq_attr_ro(scaling_max_freq);
 cpufreq_freq_attr_rw(user_scaling_min_freq);
 cpufreq_freq_attr_rw(user_scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
