f817661b1394e5d5df6a6d285bcdbd6ddc7844da
ace2nutzer <ace2nutzer@gmail.com>
2020-07-05 07:07:21 +0200

    SELinux: force permissive

--- security/selinux_n/selinuxfs.c
+++ security/selinux_n/selinuxfs.c
@@ -169,10 +169,6 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	if (copy_from_user(page, buf, count))
 		goto out;
 
-	length = -EINVAL;
-	if (sscanf(page, "%d", &new_value) != 1)
-		goto out;
-
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_ALWAYS_ENFORCE
 	// If build is user build and enforce option is set, selinux is always enforcing
