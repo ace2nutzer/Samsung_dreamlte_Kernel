5609b3e3e5ad0f4687433194dd18b01035b6bcd5
ace2nutzer <ace2nutzer@gmail.com>
2021-03-12 02:36:08 +0100

    lib/lz4: increase memory usage for EXYNOS_8895 Mongoose M2 has 32 KiB L1 data size

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -55,7 +55,11 @@
  * Reduced memory usage can improve speed, due to cache effect
  * Default value is 14, for 16KB, which nicely fits into Intel x86 L1 cache
  */
+#ifdef CONFIG_SOC_EXYNOS8895
+#define LZ4_MEMORY_USAGE 15
+#else
 #define LZ4_MEMORY_USAGE 14
+#endif
 
 #define LZ4_MAX_INPUT_SIZE	0x7E000000 /* 2 113 929 216 bytes */
 #define LZ4_COMPRESSBOUND(isize)	(\
