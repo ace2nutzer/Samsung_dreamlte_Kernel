04d469859d5293c523bb19d18b80b8a5bfcba584
ace2nutzer <ace2nutzer@gmail.com>
2023-01-25 23:55:50 +0100

    fs/inode: re-add samsung patch for RBIN

--- fs/inode.c
+++ fs/inode.c
@@ -169,7 +169,11 @@ int inode_init_always(struct super_block *sb, struct inode *inode)
 	mapping->flags = 0;
 	atomic_set(&mapping->i_mmap_writable, 0);
 #ifdef CONFIG_RBIN
-	mapping_set_gfp_mask(mapping, GFP_HIGHUSER_MOVABLE | __GFP_RBIN);
+	if ((sb->s_flags & MS_RDONLY) && !shmem_mapping(mapping))
+		mapping_set_gfp_mask(mapping, GFP_HIGHUSER_MOVABLE |
+					__GFP_RBIN);
+	else
+		mapping_set_gfp_mask(mapping, GFP_HIGHUSER_MOVABLE);
 #else
 	mapping_set_gfp_mask(mapping, GFP_HIGHUSER_MOVABLE);
 #endif
