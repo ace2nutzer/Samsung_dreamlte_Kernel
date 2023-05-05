bf63d44b915614fa4bc126e7be25dc53d853697e
ace2nutzer <ace2nutzer@gmail.com>
2023-05-05 18:32:52 +0200

    mali_kbase_js_backend: trigger kernel panic on GPU fault

--- drivers/gpu/arm/b_r16p0/backend/gpu/mali_kbase_js_backend.c
+++ drivers/gpu/arm/b_r16p0/backend/gpu/mali_kbase_js_backend.c
@@ -251,10 +251,16 @@ static enum hrtimer_restart timer_callback(struct hrtimer *timer)
 		}
 	}
 	if (reset_needed) {
+#ifdef CONFIG_A2N
+		dev_err(kbdev->dev, "JS: Job has been on the GPU for too long (JS_RESET_TICKS_SS/DUMPING timeout hit). Issueing a kernel panic ...");
+		emergency_sync();
+		panic("GPU fault");
+#else
 		dev_err(kbdev->dev, "JS: Job has been on the GPU for too long (JS_RESET_TICKS_SS/DUMPING timeout hit). Issueing GPU soft-reset to resolve.");
 
 		if (kbase_prepare_to_reset_gpu_locked(kbdev))
 			kbase_reset_gpu_locked(kbdev);
+#endif
 	}
 	/* the timer is re-issued if there is contexts in the run-pool */
 
